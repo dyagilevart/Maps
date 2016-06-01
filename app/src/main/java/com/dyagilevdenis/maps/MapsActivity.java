f5904ec09ea5ad330e413078d838f500ed2a363b
Denis Dyagilev <denis.panik@gmail.com>
2016-06-01 16:18:26 +0300

    YEah
    Now we can route between two points

--- app/src/main/java/com/dyagilevdenis/maps/MapsActivity.java
+++ app/src/main/java/com/dyagilevdenis/maps/MapsActivity.java
@@ -3,18 +3,35 @@ package com.dyagilevdenis.maps;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.text.Html;
+import android.text.Spanned;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.location.places.AutocompletePrediction;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceBuffer;
+import com.google.android.gms.location.places.Places;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -22,26 +39,50 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import butterknife.InjectView;
 
 public class MapsActivity extends FragmentActivity
         implements GoogleMap.OnMapLongClickListener
-        , GoogleMap.OnCameraChangeListener, OnMapReadyCallback {
+        , GoogleMap.OnCameraChangeListener, OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener {
 
     private GoogleMap mMap;
     private UiSettings mUiSettings;
     private int FlagForStarting = 1;
-    private  Marker From;
-    private  Marker To;
-    private Polyline mClickablePolyline;
+    private List<Polyline> mClickablePolyline = new ArrayList<>();
+    public List<Marker> markers = new ArrayList<Marker>();
+    protected LatLng LLstart;
+    protected LatLng LLend;
+    private PlaceAutocompleteAdapter mAdapter;
+    private AutoCompleteTextView mAutocompleteView;
+    private AutoCompleteTextView mAutocompleteView2;
+    private static final LatLngBounds BOUNDS = new LatLngBounds(
+            new LatLng(-34.041458, 150.790100), new LatLng(-33.682247, 151.383362));
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
     private GoogleApiClient client;
-
+    @InjectView(R.id.from) AutoCompleteTextView starting;
+    @InjectView(R.id.to) AutoCompleteTextView to;
+    @InjectView(R.id.imageButton) ImageView send;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,9 +91,19 @@ public class MapsActivity extends FragmentActivity
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        mAutocompleteView = (AutoCompleteTextView)
+                findViewById(R.id.from);
+        mAutocompleteView2 = (AutoCompleteTextView)
+                findViewById(R.id.to);
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).enableAutoManage(this,0,null).addApi(Places.GEO_DATA_API).build();
+        client.connect();
+        mAutocompleteView.setOnItemClickListener(mAutocompleteClickListener);
+        mAutocompleteView2.setOnItemClickListener(mAutocompleteClickListener2);
+        mAdapter = new PlaceAutocompleteAdapter(this, client, BOUNDS, null);
+        mAutocompleteView.setAdapter(mAdapter);
+        mAutocompleteView2.setAdapter(mAdapter);
     }
 
 
@@ -82,10 +133,7 @@ public class MapsActivity extends FragmentActivity
         mMap.setOnMyLocationChangeListener(myLocationChangeListener);
         mMap.setOnMapLongClickListener(this);
         mMap.setTrafficEnabled(true);
-    // Add a marker in Sydney and move the camera
-        //LatLng sydney = new LatLng(-34, 151);
-        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.setOnInfoWindowClickListener(this);
     }
 
     @Override
@@ -95,10 +143,17 @@ public class MapsActivity extends FragmentActivity
 
     @Override
     public void onMapLongClick(LatLng latLng) {
-            From = mMap.addMarker(new MarkerOptions().position(latLng).title("Start"));
-            //To = mMap.addMarker(new MarkerOptions().position(latLng).title("End"));
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        if (markers.size() > 1){
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "Количество маркеров максимально!", Toast.LENGTH_SHORT);
+            toast.show();
+        } else {
+            Marker marker = mMap.addMarker(new MarkerOptions().position(latLng).title("Удалить?"));
+            markers.add(marker);
+
+        }
     }
+
     private GoogleMap.OnMyLocationChangeListener myLocationChangeListener = new GoogleMap.OnMyLocationChangeListener() {
         @Override
         public void onMyLocationChange(Location location) {
@@ -112,4 +167,240 @@ public class MapsActivity extends FragmentActivity
         }
     };
 
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+        Toast toast = Toast.makeText(getApplicationContext(),
+                "Удаление!", Toast.LENGTH_SHORT);
+        toast.show();
+        markers.clear();
+        //markers.remove(marker);
+        //marker.remove();
+        mMap.clear();
+        mAutocompleteView.setText("");
+        mAutocompleteView2.setText("");
+    }
+
+    public void FindRoute(View view)
+    {
+        if(markers.size() == 2){
+            String url = getDirectionsUrl(markers.get(0).getPosition(), markers.get(1).getPosition());
+
+            DownloadTask downloadTask = new DownloadTask();
+
+            // Start downloading json data from Google Directions API
+            downloadTask.execute(url);
+        }
+        // выводим сообщение
+        Toast.makeText(this, "Вычисляю маршрут", Toast.LENGTH_SHORT).show();
+    }
+
+    private String getDirectionsUrl(LatLng origin,LatLng dest){
+
+
+        // Origin of route
+        String str_origin = "origin="+origin.latitude+","+origin.longitude;
+
+        // Destination of route
+        String str_dest = "destination="+dest.latitude+","+dest.longitude;
+
+        // Sensor enabled
+        String sensor = "sensor=false";
+
+        // Building the parameters to the web service
+        String parameters = str_origin+"&"+str_dest+"&"+sensor;
+
+        // Output format
+        String output = "json";
+
+        // Building the url to the web service
+        String url = "https://maps.googleapis.com/maps/api/directions/"+output+"?"+parameters;
+
+        return url;
+    }
+    private String downloadUrl(String strUrl) throws IOException {
+        String data = "";
+        InputStream iStream = null;
+        HttpURLConnection urlConnection = null;
+        try{
+            URL url = new URL(strUrl);
+
+            // Creating an http connection to communicate with url
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            // Connecting to url
+            urlConnection.connect();
+
+            // Reading data from url
+            iStream = urlConnection.getInputStream();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
+
+            StringBuffer sb = new StringBuffer();
+
+            String line = "";
+            while( ( line = br.readLine()) != null){
+                sb.append(line);
+            }
+
+            data = sb.toString();
+
+            br.close();
+
+        }catch(Exception e){
+
+        }finally{
+            iStream.close();
+            urlConnection.disconnect();
+        }
+        return data;
+    }
+    private class DownloadTask extends AsyncTask<String, Void, String> {
+
+        // Downloading data in non-ui thread
+        @Override
+        protected String doInBackground(String... url) {
+
+            // For storing data from web service
+            String data = "";
+
+            try{
+                // Fetching the data from web service
+                data = downloadUrl(url[0]);
+            }catch(Exception e){
+                Log.d("Background Task",e.toString());
+            }
+            return data;
+        }
+
+        // Executes in UI thread, after the execution of
+        // doInBackground()
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            ParserTask parserTask = new ParserTask();
+
+            // Invokes the thread for parsing the JSON data
+            parserTask.execute(result);
+        }
+    }
+    private class ParserTask extends AsyncTask<String, Integer, List<List<HashMap<String,String>>> > {
+
+        // Parsing the data in non-ui thread
+        @Override
+        protected List<List<HashMap<String, String>>> doInBackground(String... jsonData) {
+
+            JSONObject jObject;
+            List<List<HashMap<String, String>>> routes = null;
+
+            try {
+                jObject = new JSONObject(jsonData[0]);
+                DirectionsJSONParser parser = new DirectionsJSONParser();
+
+                // Starts parsing data
+                routes = parser.parse(jObject);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return routes;
+        }
+
+        // Executes in UI thread, after the parsing process
+        @Override
+        protected void onPostExecute(List<List<HashMap<String, String>>> result) {
+            ArrayList<LatLng> points = null;
+            PolylineOptions lineOptions = null;
+            MarkerOptions markerOptions = new MarkerOptions();
+
+            // Traversing through all the routes
+            for (int i = 0; i < result.size(); i++) {
+                points = new ArrayList<LatLng>();
+                lineOptions = new PolylineOptions();
+
+                // Fetching i-th route
+                List<HashMap<String, String>> path = result.get(i);
+
+                // Fetching all the points in i-th route
+                for (int j = 0; j < path.size(); j++) {
+                    HashMap<String, String> point = path.get(j);
+
+                    double lat = Double.parseDouble(point.get("lat"));
+                    double lng = Double.parseDouble(point.get("lng"));
+                    LatLng position = new LatLng(lat, lng);
+
+                    points.add(position);
+                }
+
+                // Adding all the points in the route to LineOptions
+                lineOptions.addAll(points);
+                lineOptions.width(5);
+                lineOptions.color(Color.CYAN);
+            }
+
+            // Drawing polyline in the Google Map for the i-th route
+            Polyline pol = mMap.addPolyline(lineOptions);
+            mClickablePolyline.add(pol);
+        }
+    }
+
+    private AdapterView.OnItemClickListener  mAutocompleteClickListener
+            = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            final AutocompletePrediction item = mAdapter.getItem(position);
+            final String placeId = item.getPlaceId();
+            PendingResult<PlaceBuffer> placeResult = Places.GeoDataApi.getPlaceById(client, placeId);
+            Toast.makeText(getApplicationContext(), "Clicked: " + placeId ,
+                    Toast.LENGTH_SHORT).show();
+            placeResult.setResultCallback(new ResultCallback<PlaceBuffer>() {
+                @Override
+                public void onResult(PlaceBuffer places) {
+                    if (!places.getStatus().isSuccess()) {
+                        // Request did not complete successfully
+                        places.release();
+                        return;
+                    }
+                    // Get the Place object from the buffer.
+                    final Place place = places.get(0);
+                    Marker marker = mMap.addMarker(new MarkerOptions().position(place.getLatLng()).title("Удалить?"));
+                    markers.add(marker);
+                    //LLstart=place.getLatLng();
+                }
+            });
+
+        }
+    };
+
+    private AdapterView.OnItemClickListener  mAutocompleteClickListener2
+            = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            final AutocompletePrediction item = mAdapter.getItem(position);
+            final String placeId = item.getPlaceId();
+            PendingResult<PlaceBuffer> placeResult = Places.GeoDataApi.getPlaceById(client, placeId);
+            Toast.makeText(getApplicationContext(), "Clicked: " + placeId ,
+                    Toast.LENGTH_SHORT).show();
+            placeResult.setResultCallback(new ResultCallback<PlaceBuffer>() {
+                @Override
+                public void onResult(PlaceBuffer places) {
+                    if (!places.getStatus().isSuccess()) {
+                        // Request did not complete successfully
+                        places.release();
+                        return;
+                    }
+                    // Get the Place object from the buffer.
+                    final Place place = places.get(0);
+                    Marker marker = mMap.addMarker(new MarkerOptions().position(place.getLatLng()).title("Удалить?"));
+                    markers.add(marker);
+                    //LLstart=place.getLatLng();
+                }
+            });
+
+        }
+    };
+
+
 }
+
